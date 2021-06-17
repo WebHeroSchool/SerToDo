9dee706fe08494915356a23ccc87874268b84cef
SerekaKen-PC <xaver1995@icloud.com>
2021-06-17 23:22:23 +0300

    Обработка событий

--- todo/src/components/ItemList/ItemList.js
+++ todo/src/components/ItemList/ItemList.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Item from '../Item/Items'
 import styles from './ItemList.module.css'
 
-const ItemList = ({itemList}) => (
+const ItemList = ({itemList, setTaskIsDone}) => (
   <div className={styles.listWrap}>
       {itemList.map(item => (
-        <Item task={item} key={item.value}/>
+        <Item
+            task={item}
+            key={item.value}
+            setTaskIsDone={setTaskIsDone}
+        />
       ))}
   </div>
 );
